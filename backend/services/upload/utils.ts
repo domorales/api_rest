01e5601479694b91a966ae29366ffd30c9a67470
domorales <domorale@espol.edu.ec>
2022-07-12 22:45:09 -0500

    DMO-FIX: Se elimina validacion con null y se quita numero magico

--- backend/services/upload/utils.ts
+++ backend/services/upload/utils.ts
@@ -2,8 +2,10 @@ import { FileArray, UploadedFile } from 'express-fileupload';
 
 import NotSendFileException from './exception/NotSendFIleExecption';
 
+const MAX_NUMBER_FILE = 1;
+
 const pathTmp = (files: FileArray): string => {
-	const isFile = files != null && Object.keys(files).length === 1,
+	const isFile = files && Object.keys(files).length === MAX_NUMBER_FILE,
 		archivo = <UploadedFile>files.archivo;
 	if (!isFile || !archivo) throw new NotSendFileException('No existe archivo que subir');
 	return archivo.tempFilePath;
